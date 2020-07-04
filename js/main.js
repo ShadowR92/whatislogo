ede47fa80560b1de732e1713d50eb8dfeb0ea70d
ShadowR92 <67151390+ShadowR92@users.noreply.github.com>
2020-07-04 13:55:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.slider_inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next">1</button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev">2</button>',
+
+
+    });
+
+
+});
